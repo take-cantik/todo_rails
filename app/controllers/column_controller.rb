6fdbdc76518330aaaf533a44ad122e26eae9945d
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-08-14 23:14:56 +0900

    Create swap column order test

--- app/controllers/column_controller.rb
+++ app/controllers/column_controller.rb
@@ -2,6 +2,7 @@ class ColumnController < ApplicationController
   def index
 		@columns = Column.order(:order)
 		@count = Column.count
+		logger.debug("なんの？")
 	end
 
 	def new
@@ -28,6 +29,36 @@ class ColumnController < ApplicationController
 	def update
 		@column = Column.find(params[:id])
 		@column.update(column_params)
+		redirect_to columns_path
+	end
+
+	def swap
+		columns = Column.order(:order)
+		count = params[:count]
+		count = count.to_i
+
+		columns.each_with_index do |column, index|
+			logger.debug("----------------------")
+			logger.debug(count)
+			logger.debug(index)
+			if count == index then
+				@left_column = column
+				logger.debug("left_column:")
+				logger.debug(@left_column)
+				logger.debug(@left_column.order)
+			elsif count + 1 == index then
+				@right_column = column
+				logger.debug("right_column:")
+				logger.debug(@right_column)
+				logger.debug(@right_column.order)
+			end
+		end
+
+		@left_column.order, @right_column.order = @right_column.order, @left_column.order
+
+		@left_column.update(column_params)
+		@right_column.update(column_params)
+
 		redirect_to columns_path
 	end
 
