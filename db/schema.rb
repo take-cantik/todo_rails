3c1dda736d840d407b4b3bd311326ee7e4f52e99
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-08-17 16:32:39 +0900

    Generate model comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_071313) do
+ActiveRecord::Schema.define(version: 2021_08_17_072959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 2021_08_17_071313) do
   create_table "columns", force: :cascade do |t|
     t.string "name"
     t.integer "order"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "card_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
