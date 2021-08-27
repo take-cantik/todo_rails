e00a108f2f4c107adee8b233ca483d1580222480
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-08-28 02:17:51 +0900

    Setting google oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
+
   root 'home#index'
 
   get '/columns', to: 'column#index'
