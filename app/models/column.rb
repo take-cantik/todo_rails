b65048443bd1e58d427a6d71ce069c7a6bd927d6
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-08-31 04:46:31 +0900

    Create class method to get columns to be swap

--- app/models/column.rb
+++ app/models/column.rb
@@ -1,4 +1,27 @@
 class Column < ApplicationRecord
   has_many :cards, -> { order('deadline') }
   auto_increment :order
+
+  def self.get_swap_columns(count, right_left)
+    @columns = self.order(:order)
+
+    @columns.each_with_index do |column, index|
+      if count == index then
+        @original_column = column
+      end
+
+      if right_left == 0 then
+        if count + 1 == index then
+          @change_column = column
+        end
+      elsif right_left == 1 then
+        if count - 1 == index then
+          @change_column = column
+        end
+      end
+    end
+
+    return @original_column, @change_column
+  end
+
 end
