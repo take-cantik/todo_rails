0ee69cda3ca34d2e03ed5f4ff1d2dc77d2ee304f
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-08-31 02:32:58 +0900

    Add user_id column to Column model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_165449) do
+ActiveRecord::Schema.define(version: 2021_08_30_173211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_08_27_165449) do
     t.integer "order"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
