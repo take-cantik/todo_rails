5d70a21f01d8da55638a473c9474fff419438c02
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-09-07 13:51:06 +0900

    Get res using axios

--- app/javascript/packs/views/User.vue
+++ app/javascript/packs/views/User.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
     <h1>This is User Page</h1>
+    <div v-for="user in users" :key="user.id">
+      <h2>{{ user.name }}</h2>
+    </div>
     <router-link to='/'>ahiahi</router-link>
     <a href="/">ahi</a>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+  data () {
+    return {
+      users: []
+    }
+  },
+  mounted () {
+    axios
+      .get('/api/user.json')
+      .then(response => (this.users = response.data))
+  }
 }
 </script>
 
