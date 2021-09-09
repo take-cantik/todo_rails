f0153f79654dd69e030fab2b505f54c5f2940854
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-09-09 16:59:47 +0900

    Rename view files to pages

--- app/javascript/packs/router/index.js
+++ app/javascript/packs/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import User from '../views/User.vue'
+import Home from '../views/home.vue'
+import User from '../views/users.vue'
 
 Vue.use(VueRouter)
 
